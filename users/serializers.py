3adcb556b166332f917123ab252fe0b96cd89143
faignaghi <faignaghi@gmail.com>
2022-11-10 01:37:50 +0100

    commit

--- users/serializers.py
+++ users/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers, validators
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User                                     # same
+from django.conf import settings                                                # same
 from django.contrib.auth.password_validation import validate_password
 
 class RegisterSerializer(serializers.ModelSerializer):
@@ -24,7 +25,7 @@ class RegisterSerializer(serializers.ModelSerializer):
     
     
     class Meta:
-        model = User
+        model = settings.AUTH_USER_MODEL
         fields = (
             'username',
             'email',
@@ -33,3 +34,20 @@ class RegisterSerializer(serializers.ModelSerializer):
             'password',
             'password1'
         )
+        
+        
+        
+    def validate(self, data):
+        if data['password'] != data['password']:
+            raise serializers.ValidationError(
+                {"pasword: Password didn't match.... "}
+            )
+        return data
+    
+    def create(self, validated_data):
+        password = validated_data.pop("password")
+        validated_data.pop('password1')
+        user = User.objects.create(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
